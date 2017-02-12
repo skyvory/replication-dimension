4c28b2440c26bc62c88bc4c04f6f94e46f731f21
Rika Voice <skyvory@hotmail.com>
2017-02-12 16:22:08 +0800

    Update 4chan parser
    4chan.org is now using their own cdn domain for media files

--- app/Parsers/FourchanParser.php
+++ app/Parsers/FourchanParser.php
@@ -13,10 +13,13 @@ class FourchanParser
         is((0-9)|\w*?).4chan.org\/\S+\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)
         (0-9|\w*?) : optional string after is and before . (dot)
         */
-        preg_match_all("#is((0-9)|\w*?).4chan.org\/\S+\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        // preg_match_all("#is((0-9)|\w*?).4chan.org\/\S+\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        // preg_match_all("#((0-9)|\w*?).4cdn.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        preg_match_all("#<a href=\"\/\/i.4cdn.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
 
         foreach ($match[0] as $key => $value) {
-            $value = 'http://' . $value;
+            $value = substr($value, 11);
+            $value = 'https://' . $value;
             if(!in_array($value, $image_list)) {
                 $image_list[] = $value;
             }
