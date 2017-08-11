1c2869b3444f0ac830efff816e54ca0bb617148d
Rika Voice <skyvory@hotmail.com>
2017-08-11 15:51:24 +0800

    Add true proxy constant

--- config/constant.php
+++ config/constant.php
@@ -1,7 +1,9 @@
 <?php
 return [
-    'USE_PROXY' => true,
-    'PROXY_URL' => 'https://quantum.000webhostapp.com/',
+    'USE_PROXY' => false,
+    'PROXY_URL' => 'https://theskyvory.net/',
     'USE_REQUEST_ENCRYPTION' => true,
-    'ENCRYPTION_HASH' => '16a6d7f49404004f737be38f9caec915a411a5380ea1604edbaf34ebc398f6a4'
+    'ENCRYPTION_HASH' => '16a6d7f49404004f737be38f9caec915a411a5380ea1604edbaf34ebc398f6a4',
+    'TRUE_PROXY_ADRESS' => '144.217.242.194:8080',
+    'USE_TRUE_PROXY' => false
 ];
