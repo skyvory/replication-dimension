d3107a4e6bf8f6457b98a722bac5cc47268986f7
Rika Voice <skyvory@hotmail.com>
2019-03-12 18:03:09 +0800

    Update 4chan parser

--- app/Parsers/FourchanParser.php
+++ app/Parsers/FourchanParser.php
@@ -16,7 +16,8 @@ class FourchanParser
         // preg_match_all("#is((0-9)|\w*?).4chan.org\/\S+\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
         // preg_match_all("#((0-9)|\w*?).4cdn.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
         // preg_match_all("#<a href=\"\/\/i.4cdn.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
-        preg_match_all("#href=\"\/\/(is2|is3).4chan.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        // preg_match_all("#href=\"\/\/(is2|is3).4chan.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        preg_match_all("#href=\"\/\/i.4cdn.org\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
 
         foreach ($match[0] as $key => $value) {
             $value = substr($value, 8);
