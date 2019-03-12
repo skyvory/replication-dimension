f84c28b98b264890f084f21e250b24b3c7767cf9
Rika Voice <skyvory@hotmail.com>
2019-03-12 18:05:28 +0800

    Update desuarchive parser

--- app/Parsers/DesuarchiveParser.php
+++ app/Parsers/DesuarchiveParser.php
@@ -7,7 +7,7 @@ class DesuarchiveParser
     {
         $image_list = array();
 
-        preg_match_all("#https?:\/\/desu-usergeneratedcontent.xyz\/\S+\/image\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
+        preg_match_all("#https?:\/\/s1.desu-usergeneratedcontent.xyz\/\S+\/image\/\S+\.(jpg|png|gif|webp|jpeg|bmp|webm)#", $html_content, $match);
 
         foreach($match[0] as $key => $value) {
             if(!in_array($value, $image_list)) {
